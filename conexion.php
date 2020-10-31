c096c249d32262e077cd582da809d9129aa11a53
Miguel Angel <michirapper2001@gmail.com>
2020-10-31 11:46:53 +0100

    pag login

---
+++ conexion.php
@@ -0,0 +1,4 @@
+<?php
+	$conexion = mysqli_connect("localhost", "root", "", "bdfase2g8") or trigger_error("No se puede conectar con el servidor",E_USER_ERROR); 
+    mysqli_set_charset ($conexion,"utf8"); 	
+?>
---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css_Login.css">
+    <?php
+    require_once("conexion.php");
+    ?>
+</head>
+
+<body>
+<div class="sidenav">
+         <div class="login-main-text">
+            <h2>Macaquinho ERP</h2>
+            <p>El ERP mas simple que veras.</p>
+            <img src="./img/LogoERP.png" alt="" class="img-fluid ">
+         </div>
+      </div>
+      <div class="main">
+         <div class="col-md-6 col-sm-12">
+            <div class="login-form">
+               <form>
+                  <div class="form-group">
+                     <label>Usuario</label>
+                     <input type="text" class="form-control" placeholder="Usuario">
+                  </div>
+                  <div class="form-group">
+                     <label>Contraseña</label>
+                     <input type="password" class="form-control" placeholder="Contraseña">
+                  </div>
+                  <button type="submit" class="btn btn-black">Login</button>
+                  <button type="submit" class="btn btn-secondary">Register</button>
+               </form>
+            </div>
+         </div>
+      </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
+</html>
